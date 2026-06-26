366d99c7dd6709d1a7e5a09dae754d07511784c8
José Romário Viana Filho <joseromario.viana@jucemg.mg.gov.br>
2026-06-26 14:09:23 -0300

    conf: teste bootstrap-repo
    This script defines a function to trigger a GitHub Actions workflow for repository setup using user inputs.

---
+++ app.js
@@ -0,0 +1,32 @@
+const TOKEN = "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"; // ⚠️ por enquanto
+
+async function createRepo() {
+  const repo = document.getElementById("repo").value;
+  const area = document.getElementById("area").value;
+  const domain = document.getElementById("domain").value;
+
+  const response = await fetch(
+    "https://api.github.com/repos/junta-comercial-mg/devops-tools/actions/workflows/bootstrap-repo.yml/dispatches",
+    {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${TOKEN}`,
+        "Accept": "application/vnd.github+json"
+      },
+      body: JSON.stringify({
+        ref: "main",
+        inputs: {
+          repo_name: repo,
+          area: area,
+          domain: domain
+        }
+      })
+    }
+  );
+
+  if (response.ok) {
+    alert("✅ Workflow disparado!");
+  } else {
+    alert("❌ Erro ao disparar workflow");
+  }
+}
